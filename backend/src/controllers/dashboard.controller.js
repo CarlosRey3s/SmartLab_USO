0229542fe4061030f2e1d811a1a0614ab17d877a
AstridPerezGaray <astridperezg3@gmai.com>
2026-07-24 15:19:44 -0600

    no se que arregle pero se arreglo algo

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -1,70 +1,37 @@
 const dashboardService = require("../services/dashboard.service");
 
-
 class DashboardController {
 
+    async getDashboard(req, res) {
 
-    async getDashboard(req,res){
-
-
-        try{
-
+        try {
 
             const data = {
-
-
-                kpis:
-                await dashboardService.getKPIs(),
-
-
-                reservas:
-                await dashboardService.getReservasSemana(),
-
-
-                alertas:
-                await dashboardService.getAlertas(),
-
-
-                saturacion:
-                await dashboardService.getSaturacion(),
-
-
-                agenda:
-                await dashboardService.getAgenda()
-
-
+                kpis: await dashboardService.getKPIs(),
+                reservas: await dashboardService.getReservasSemana(),
+                alertas: await dashboardService.getAlertas(),
+                saturacion: await dashboardService.getSaturacion(),
+                agenda: await dashboardService.getAgenda()
             };
 
+            return res.status(200).json(data);
 
+        } catch (error) {
 
-            res.json(data);
+            console.error("==================================");
+            console.error("ERROR AL CARGAR EL DASHBOARD");
+            console.error(error);
+            console.error("==================================");
 
-
-
-        }catch(error){
-
-
-            console.error(
-                "Error dashboard:",
-                error
-            );
-
-
-            res.status(500).json({
-
-                message:"Error cargando dashboard"
-
+            return res.status(500).json({
+                success: false,
+                message: error.message
             });
-
 
         }
 
-
     }
 
-
 }
-
-
 
 module.exports = new DashboardController();
