7ac17f7f2ca5ef8ef23b6898cc02eedc687500fb
Carlos <rc21i04001@usonsonate.edu.sv>
2026-07-25 22:06:41 -0600

    Refactor estudianteDashboard.service.ts to streamline imports and update API_URL definition

--- frontend/src/services/estudianteDashboard.service.ts
+++ frontend/src/services/estudianteDashboard.service.ts
@@ -1,27 +1,14 @@
 import axios from "axios";
+import type {   EstudianteDashboard} from "../types/estudianteDashboard.types";
+import { BASE_URL } from "../config/api";
 
-import type {
-    EstudianteDashboard
-} from "../types/estudianteDashboard.types";
-
-
-
-const API_URL =
-"http://localhost:4000/api/estudiante/dashboard";
-
-
+const API_URL = `${BASE_URL}/api/estudiante/dashboard`;
 
 export const obtenerDashboardEstudiante =
 async (): Promise<EstudianteDashboard> => {
-
-
     const response =
     await axios.get(
         API_URL
     );
-
-
     return response.data;
-
-
 };
